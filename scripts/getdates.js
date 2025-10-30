cdb5ac80555e17857859e8dd8eca6b8c0b2778ef
Katlin Denney <kdenney@byupathway.edu>
2025-10-30 14:02:37 -0600

    dates yay

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
 
-const lastModified = new Date(document.lastModified);
-
 const options = {
     weekday:'long',
     year:'numeric',
@@ -11,8 +9,24 @@ const options = {
     hour12: true,
     timeZoneName:'short'             
 };
-const formattedDate = lastModified.toLocaleString('en-US', options);
 
+
+//this for the copyright year
+const currentYear = new Date().getFullYear();
+document.getElementById('year').textContent = currentYear;
+
+//this for the current date
+const currentDate = new Date();
+const formattedNow = currentDate.toLocaleString('en-US', options);
+document.getElementById('date').textContent = formattedNow.replace(',', '');
+
+//this for the last modified date
+const lastModified = new Date(document.lastModified);
+const formattedDate = lastModified.toLocaleString('en-US', options);
 document.getElementById('lastModified').textContent = formattedDate.replace(',',' ');
+
+
+
+
 
 
